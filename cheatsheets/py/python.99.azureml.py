efe18e5a96f81500c566cea23cf0a4437ed5e728
questsin <questsin@gmail.com>
2020-02-20 14:40:14 -0500

    azureml sdk

--- cheatsheets/py/python.99.azureml.py
+++ cheatsheets/py/python.99.azureml.py
@@ -1,5 +1,43 @@
 from azureml import Workspace
 
 ws = Workspace()
+ws = Workspace.from_config()
+
 ds = ws.datasets['*.csv']
 df = ds.to_dataframe()
+
+#ws.name
+#ws.location
+#ws.subscription_id
+#ws.resource_group
+
+from azureml.core import Enviornment
+env = Enviornment.get(workspace=ws, nmae="qwer")
+env.get_image_details(ws)
+
+from azureml.core.environment import CondaDependencies
+myenv = Environment(name="myenv")
+conda_dep = CondaDependencies()
+conda_dep.add_conda_package("scikit-learn")
+
+from azureml.core import Dataset
+dataset = Dataset.Tabular.from_delimited_files(path = [(datastore, 'train-dataset/tabular/iris.csv')])
+# preview the first 3 rows of the dataset
+dataset.take(3).to_pandas_dataframe()
+
+from azureml.core.compute import ComputeTarget, AmlCompute
+   from azureml.core.compute_target import ComputeTargetException
+
+   # Choose a name for your CPU cluster
+   cpu_cluster_name = "cpu-cluster"
+
+   # Verify that cluster does not exist already
+   try:
+       cpu_cluster = ComputeTarget(workspace=ws, name=cpu_cluster_name)
+       print('Found existing cluster, use it.')
+   except ComputeTargetException:
+       compute_config = AmlCompute.provisioning_configuration(vm_size='STANDARD_D2_V2',
+                                                              max_nodes=4)
+       cpu_cluster = ComputeTarget.create(ws, cpu_cluster_name, compute_config)
+
+   cpu_cluster.wait_for_completion(show_output=True)
