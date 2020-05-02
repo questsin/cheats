34aeea150be8fb45e2dfa3f4f94a4d76df4c7d88
questsin <questsin@gmail.com>
2020-05-02 15:45:06 -0400

    better server

--- cheatsheets/js/www.js
+++ cheatsheets/js/www.js
@@ -2,29 +2,39 @@ var os = require('os');
 var http = require('http');
 var url = require('url');
 
+var server = {};
+var metrics = {};
+var clients = {};
+var requests = {};
+var responses = {};
+var consoles = {};
+consoles.log = {};
+consoles.info = {};
+consoles.err = {};
+
 http.createServer(function (request, response) {
   response.setHeader('Content-Type', 'application/json');
-  object = {};
-  object.info = {"app":"nodejs"};
-  object.info.platform = os.platform();
-  object.info.release = os.release();
-  object.info.tmpdir = os.tmpdir();
-  object.info.type = os.type();
-  object.info.uptime = os.uptime();
-  object.info.userInfo = os.userInfo();
-  object.info.arch = process.arch;
-  object.headers = request.headers;
-  object.method = request.method;
-  object.url = request.url;
-  object.urlparse = url.parse(request.url, true);
-  object.body = {};
-  object.os = os;
-  object.versions = process.versions;
-  object.process = {};
-  object.process.argv = process.argv;
-  object.process.execPath = process.execPath;
-  object.process.cwd = process.cwd();
-  object.env = process.env;
+  server.info = {"app":"nodejs"};
+  server.info.platform = os.platform();
+  server.info.release = os.release();
+  server.info.tmpdir = os.tmpdir();
+  server.info.type = os.type();
+  server.info.uptime = os.uptime();
+  server.info.userInfo = os.userInfo();
+  server.info.arch = process.arch;
+  server.os = os;
+  server.versions = process.versions;
+  server.process = {};
+  server.process.argv = process.argv;
+  server.process.execPath = process.execPath;
+  server.process.cwd = process.cwd();
+  server.env = process.env;
+
+  requests.headers = request.headers;
+  requests.method = request.method;
+  requests.url = request.url;
+  requests.urlparse = url.parse(request.url, true);
+  requests.body = {};
 
   let body = [];
   request.on('error', (err) => {
@@ -33,7 +43,14 @@ http.createServer(function (request, response) {
     body.push(chunk);
   }).on('end', () => {
     body = Buffer.concat(body).toString();
-    object.body = body;
+    requests.body = body;
+    object = {};
+    object.server = server;
+    object.metrics = metrics;
+    object.requests = requests;
+    object.responses = responses;
+    object.clients = clients;
+    object.consoles = consoles;
     response.write(JSON.stringify(object, undefined, 4));
     response.end(); //end the response
     });
