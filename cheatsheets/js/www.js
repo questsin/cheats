3a0c821f9e59c23e610336a85b97acf07b7dabb3
questsin <questsin@gmail.com>
2020-05-02 15:56:32 -0400

    meta

--- cheatsheets/js/www.js
+++ cheatsheets/js/www.js
@@ -3,7 +3,7 @@ var http = require('http');
 var url = require('url');
 
 var server = {};
-var metrics = {};
+var metrics = {requests:0, bodies:0, errors:0, ends:0};
 var clients = {};
 var requests = {};
 var responses = {};
@@ -12,36 +12,41 @@ consoles.log = {};
 consoles.info = {};
 consoles.err = {};
 
+server.info = {"app":"nodejs"};
+server.info.platform = os.platform();
+server.info.release = os.release();
+server.info.tmpdir = os.tmpdir();
+server.info.type = os.type();
+server.info.uptime = os.uptime();
+server.info.userInfo = os.userInfo();
+server.info.arch = process.arch;
+server.os = os;
+server.versions = process.versions;
+server.process = {};
+server.process.argv = process.argv;
+server.process.execPath = process.execPath;
+server.process.cwd = process.cwd();
+server.env = process.env;
+
 http.createServer(function (request, response) {
   response.setHeader('Content-Type', 'application/json');
-  server.info = {"app":"nodejs"};
-  server.info.platform = os.platform();
-  server.info.release = os.release();
-  server.info.tmpdir = os.tmpdir();
-  server.info.type = os.type();
-  server.info.uptime = os.uptime();
-  server.info.userInfo = os.userInfo();
-  server.info.arch = process.arch;
-  server.os = os;
-  server.versions = process.versions;
-  server.process = {};
-  server.process.argv = process.argv;
-  server.process.execPath = process.execPath;
-  server.process.cwd = process.cwd();
-  server.env = process.env;
 
   requests.headers = request.headers;
   requests.method = request.method;
   requests.url = request.url;
   requests.urlparse = url.parse(request.url, true);
   requests.body = {};
+  metrics.requests +=1;
 
   let body = [];
   request.on('error', (err) => {
+    metrics.errors +=1;
     console.error(err);
   }).on('data', (chunk) => {
+    metrics.bodies+=1;
     body.push(chunk);
   }).on('end', () => {
+    metrics.ends +=1;
     body = Buffer.concat(body).toString();
     requests.body = body;
     object = {};
